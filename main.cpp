7aa143651ac998f4ae5f7e018a9f4d44ad8748ed
jairo <jairosp2000@gmail.com>
2025-07-20 11:24:11 +0200

    First commit 20/Jul/2025

--- main.cpp
+++ main.cpp
@@ -1,31 +1,45 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-    int TC;
-    cin >> TC;
+string next_dir(const string& current, const string& bend) {
+    if (bend == "No") return current;
 
-    while (TC--) {
-        int water = 0;
-        stack<int> st;
-
-        string line;
-        cin >> line;
-
-        for (int i = 0; i < line.size(); i++) {
-            char ch = line[i];
-            if (ch == '_') {
-                continue;
-            } else if (ch == '\\') {
-                st.push(i);
-            } else if (ch == '/') {
-                if (!st.empty()) {
-                    int last = st.top();
-                    st.pop();
-                    water += i - last;
-                }
-            }
-        }
-        cout << water << endl;
+    if (current == "+x") return bend;
+    if (current == "-x") {
+        if (bend[0] == '+') return "-" + bend.substr(1);
+        else return "+" + bend.substr(1);
     }
+    if (current == "+y") {
+        if (bend == "+y") return "-x";
+        if (bend == "-y") return "+x";
+    }
+    if (current == "-y") {
+        if (bend == "+y") return "+x";
+        if (bend == "-y") return "-x";
+    }
+    if (current == "+z") {
+        if (bend == "+z") return "-x";
+        if (bend == "-z") return "+x";
+    }
+    if (current == "-z") {
+        if (bend == "+z") return "+x";
+        if (bend == "-z") return "-x";
+    }
+
+    return current;
+}
+
+int main() {
+    int L;
+    while (cin >> L, L) {
+        string current = "+x";
+        for (int i = 1; i < L; ++i) {
+            string bend;
+            cin >> bend;
+            current = next_dir(current, bend);
+        }
+        cout << current << '\n';
+    }
+    return 0;
 }
